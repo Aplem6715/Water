df8756413b0aeddb2e5305214e843a8e932615c7
sato.d <sato.daichi@appci.jp>
2023-12-04 14:21:14 +0900

    ComputeShader版SPH実装（グリッド分割無し）

--- Assets/Scripts/Compute/ComputeShaderSPH.cs
+++ Assets/Scripts/Compute/ComputeShaderSPH.cs
@@ -12,6 +12,12 @@ namespace SPH.Compute
         [SerializeField] private ComputeShader _shader;
         [SerializeField] private RenderTexture _liquidTex;
         [SerializeField] private int2 NumParticles;
+        [SerializeField] private int2 FieldSize;
+        [SerializeField] int WallThickness = 4;
+        [SerializeField] int ParticleRenderSize = 1;
+
+        private int _numParticles;
+        private int _numWalls;
 
         private int _kernelSpawn;
         private int _kernelPressureDensity;
@@ -25,12 +31,14 @@ namespace SPH.Compute
         {
             float2 pos;
             float2 v;
-            float2 accel;
+            float2 v2;
             float density;
             float pressure;
+            int isStatic;
+            int isActive;
         }
 
-        private void Start()
+        private void Awake()
         {
             _kernelSpawn = _shader.FindKernel("SpawnParticle");
             _kernelPressureDensity = _shader.FindKernel("UpdatePressure");
@@ -41,9 +49,16 @@ namespace SPH.Compute
             SetupBuffers();
         }
 
+        private void Start()
+        {
+            _shader.Dispatch(_kernelSpawn, 1, 1, 1);
+        }
+
         private void SetupBuffers()
         {
-            _particleBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, NumParticles.x * NumParticles.y, Marshal.SizeOf<Particle>());
+            _numParticles = NumParticles.x * NumParticles.y;
+            _numWalls = WallThickness * 2 * (FieldSize.x + WallThickness * 2) + WallThickness * 2 * FieldSize.y;
+            _particleBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _numParticles + _numWalls, Marshal.SizeOf<Particle>());
 
             _shader.SetBuffer(_kernelSpawn, "_Particles", _particleBuffer);
             _shader.SetBuffer(_kernelPressureDensity, "_Particles", _particleBuffer);
@@ -54,9 +69,14 @@ namespace SPH.Compute
             _shader.SetTexture(_kernelRender, "_LiquidTex", _liquidTex);
             _shader.SetTexture(_kernelClear, "_LiquidTex", _liquidTex);
 
-            _shader.SetInt("_numParticles", NumParticles.x * NumParticles.y);
+            _shader.SetInt("_numParticles", _numParticles);
             _shader.SetInt("_numParticleX", NumParticles.x);
             _shader.SetInt("_numParticleY", NumParticles.y);
+            _shader.SetInt("_fieldWidth", FieldSize.x);
+            _shader.SetInt("_fieldHeight", FieldSize.y);
+            _shader.SetInt("_numParticleAndWall", _numParticles + _numWalls);
+            _shader.SetInt("_WallThickness", WallThickness);
+            _shader.SetInt("_ParticleRenderSize", ParticleRenderSize);
         }
 
         private void Update()
@@ -71,12 +91,19 @@ namespace SPH.Compute
 
         private void FixedUpdate()
         {
-            _shader.SetFloat("_dt", Time.deltaTime);
+            _shader.SetFloat("_dt", 0.0005f);
 
-            _shader.Dispatch(_kernelPressureDensity, (int)math.ceil(NumParticles.x * NumParticles.y / 64.0), 1, 1);
-            _shader.Dispatch(_kernelForceMove, (int)math.ceil(NumParticles.x * NumParticles.y / 64.0), 1, 1);
-            _shader.Dispatch(_kernelRender, (int)math.ceil(NumParticles.x * NumParticles.y / 64.0), 1, 1);
             _shader.Dispatch(_kernelClear, _liquidTex.width / 8, _liquidTex.height / 8, 1);
+
+            int numThreadGroup = (int)math.ceil((_numParticles + _numWalls) / 64.0);
+            _shader.Dispatch(_kernelPressureDensity, numThreadGroup, 1, 1);
+            _shader.Dispatch(_kernelForceMove, numThreadGroup, 1, 1);
+            _shader.Dispatch(_kernelRender, numThreadGroup, 1, 1);
+        }
+
+        private void OnDestroy()
+        {
+            _particleBuffer.Dispose();
         }
     }
 }
